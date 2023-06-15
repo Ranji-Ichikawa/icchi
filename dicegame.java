0e6fb9cd53c47f2d2cc9adf68f375ba24273f5cd
Ranji Ichikawa <s1300193@u-aizu.ac.jp>
2023-06-15 15:37:46 +0900

    Greetings added

--- dicegame.java
+++ dicegame.java
@@ -1,8 +1,10 @@
 import java.util.Random;
+import java.util.scanner;
 
 public class DiceGame {
     public static void main(String[] args) {
         rollDice();
+        greeting();
     }
 
     public static void rollDice() {
@@ -13,5 +15,16 @@ public class DiceGame {
         System.out.println("Die 1: " + die1);
         System.out.println("Die 2: " + die2);
         System.out.println("Total value: " + (die1 + die2));
+    }
+
+    public static void greeting() {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("What is your name?");
+        
+        String name = scanner.nextLine();
+        
+        System.out.println("Hello, " + name + "!")
     }
 }
