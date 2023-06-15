a6b56560bf82a68d4ff6f16ce5c0865e8553cab4
Ranji Ichikawa <s1300193@u-aizu.ac.jp>
2023-06-15 15:50:28 +0900

    Won/lost message added

--- dicegame.java
+++ dicegame.java
@@ -13,5 +13,7 @@ public class DiceGame {
         System.out.println("Die 1: " + die1);
         System.out.println("Die 2: " + die2);
         System.out.println("Total value: " + (die1 + die2));
+        if ((die1 + die2) >= 7 ) System.out.println("you won!");
+        else System.out.println("you lost");
     }
 }
